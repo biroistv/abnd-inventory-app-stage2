a2c63ab573105da7d340ddd346f671956101ffb7
Biró István <biroistv@outlook.hu>
2018-08-18 17:36:28 +0200

    Product change dialog added
    #added
    - dialog to notify the user if there is any unsaved change left
    
    #changed
    
    #removed
    
    #misc

--- app/src/main/java/com/example/biro/inventoryapp/EditActivity.java
+++ app/src/main/java/com/example/biro/inventoryapp/EditActivity.java
@@ -1,13 +1,18 @@
 package com.example.biro.inventoryapp;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +40,8 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
     @BindView(R.id.supp_phone_edittext)
     EditText suppPhoneEditText;
 
+    private boolean productHasChanged = false;
+
     private static final int EXISTING_PRODUCT_LOADER = 0;
     private Uri mCurrentProductUri;
 
@@ -52,7 +59,15 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
+        // Binding the views with the butterKnife
         ButterKnife.bind(this);
+
+        // Setting up the tuch listener on each editTextView
+        nameEditText.setOnTouchListener(touchListener);
+        priceEditText.setOnTouchListener(touchListener);
+        quantityEditText.setOnTouchListener(touchListener);
+        suppNameEditText.setOnTouchListener(touchListener);
+        suppPhoneEditText.setOnTouchListener(touchListener);
 
         Intent intent = getIntent();
         mCurrentProductUri = intent.getData();
@@ -148,5 +163,73 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
                 quantityEditText.getText().toString(),
                 suppNameEditText.getText().toString(),
                 suppPhoneEditText.getText().toString());
+    }
+
+    private View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            productHasChanged = true;
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                if (!productHasChanged){
+                    NavUtils.navigateUpFromSameTask(EditActivity.this);
+                    return true;
+                }
+
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // User clicked "Discard" button, navigate to parent activity.
+                                NavUtils.navigateUpFromSameTask(EditActivity.this);
+                            }
+                        };
+
+                // Show a dialog that notifies the user they have unsaved changes
+                showUnsavedChangesDialog(discardButtonClickListener);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_dialog_msg);
+        builder.setPositiveButton(R.string.discard_msg, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing_msg, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (dialog != null)
+                    dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!productHasChanged) {
+            super.onBackPressed();
+            return;
+        }
+
+        DialogInterface.OnClickListener discardButtonClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        };
+
+        showUnsavedChangesDialog(discardButtonClickListener);
     }
 }
